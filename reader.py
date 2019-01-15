5c1153da388cdf285f163c201725b0b6216da2a0
Mario Schuetz <mayo2@gmx.de>
2019-01-15 14:25:32 +0100

    Extract data from csv

---
+++ reader.py
@@ -0,0 +1,57 @@
+import csv
+
+file = 'btw17_kerg.csv'
+
+def get_content(line):
+	print (line)
+	print (str(len(line)) + ' entries')
+
+def create_list(headers, data):
+	list = []
+	i = 3
+
+	while i < len(data):
+		who = {
+			'name': headers[i],
+			'first': {
+				'current': data[i],
+				'previous': data[i + 1]
+				},
+			'second': {
+				'current': data[i + 2],
+				'previous': data[i + 3]
+			}
+		}
+		list.append(who)
+		i += 4
+
+	return list
+
+
+def csv_reader():
+	collection = []
+	headers = []
+	try:
+		with open(file) as f:
+			reader = csv.reader(f, delimiter = ';')
+			for line in reader:
+				if reader.line_num == 1:
+					headers = line
+
+				elif reader.line_num >= 4:
+					region = {
+						id: line[0],
+						'name': line[1],
+						'belongs_to': line[2],
+						'results': create_list(headers, line)
+
+					}
+					collection.append(region)
+	except csv.Error as e:
+		print(e)
+		sys.exit(-1)
+
+	print('Header entries: ' + str(len(headers)))
+	print(headers)
+
+csv_reader()
