64d25e978806b78a623cd03f5942b3a82d26a684
Mario Schuetz <mayo2@gmx.de>
2019-01-16 21:40:18 +0100

    Create tables

--- btw17_main.py
+++ btw17_main.py
@@ -1,33 +1,36 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Integer, Column, String
+from sqlalchemy import Integer, Column, String, ForeignKey
 
 engine = create_engine('sqlite:///btw_results.db')
 Session = sessionmaker(bind = engine)
 session = Session()
 
 Base = declarative_base ()
-class Constituencies(Base):
+class Constituency(Base):
 	__tablename__ = 'constituencies'
 	id = Column(Integer, primary_key = True)
 	belongsTo = Column(Integer)
 	name = Column(String)
+	parties = relationship('Parties')
 
-class Parties(Base):
+class Party(Base):
 	__tablename__ = 'parties'
 	id = Column(Integer, primary_key = True)
-	id_c = Column(Integer)
 	name = Column(String)
+	constituency_id = Column(Integer, ForeignKey('constituencies.id'))
+	constituencies = relationship('Constituencies')
 
-class Votes(Base):
+class Vote(Base):
 	__tablename__ = 'votes'
 	id = Column(Integer, primary_key = True)
-	id_p = Column(Integer)
 	e_current = Column(Integer)
 	e_previous = Column(Integer)
 	p_current = Column(Integer)
 	p_previous = Column(Integer)
+	party_id = Column(Integer, ForeignKey('parties.id'))
+	parties = relationship('Parties')
 
 # generate the DB schema automatically
 Base.metadata.create_all(engine)
