6d3b1b879dafec6b1edbcd8a1b95fce2b8d18fba
Mario Schuetz <mayo2@gmx.de>
2019-01-17 00:34:37 +0100

    Add data collection to dump

--- btw17_utils.py
+++ btw17_utils.py
@@ -1,5 +1,6 @@
 from flask import g
 import sqlite3
+import json
 
 def db_connect():
 	return sqlite3.connect('btw_results.sqlite3')
@@ -7,17 +8,17 @@ def db_connect():
 def db_close(db):
 	db.close()
 
-def get_db () :
-	if not hasattr (g, ’sqlite_db’) :
-		db = sqlite3.connect(’btw17.db’)
+def get_db() :
+	if not hasattr (g, 'sqlite_db') :
+		db = sqlite3.connect('btw17.db')
 		g.sqlite_db = db
 	return g.sqlite_db
 
-@app.teardown_appcontext
+# @app.teardown_appcontext
 def close_db(error) :
-	if hasattr (g, ’sqlite_db’):
+	if hasattr (g, 'sqlite_db'):
 		g.sqlite_db.close()
 
-def json_dump(filename):	
+def json_dump(filename, data):	
 	with open(filename, 'w') as outfile:
-    	json.dump(collection, outfile, indent=4, default=str)
+		json.dump(data, outfile, indent=4, default=str)
