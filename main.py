5ab931af0720c45d158b704371ccda3098a6a3ec
Mario Schuetz <mayo2@gmx.de>
2019-01-17 19:04:47 +0100

    Restructre database. Avoid party duplicates

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Integer, Column, String, ForeignKey
 from reader import csv_reader
@@ -23,14 +24,12 @@ class County(Base):
 	belongs_to = Column(Integer, ForeignKey('provinces.id'))
 	name = Column(String)
 	provinces = relationship('Province')
-	parties = relationship('Party')
+	results = relationship('Result')
 
 class Party(Base):
 	__tablename__ = 'parties'
 	id = Column(Integer, primary_key = True)
 	name = Column(String)
-	county_id = Column(Integer, ForeignKey('counties.id'))
-	counties = relationship('County')
 	results = relationship('Result')
 
 class Result(Base):
@@ -42,31 +41,44 @@ class Result(Base):
 	z_previous = Column(Integer)
 	party_id = Column(Integer, ForeignKey('parties.id'))
 	parties = relationship('Party')
+	county_id = Column(Integer, ForeignKey('counties.id'))
+	counties = relationship('County')
 
 # generate the DB schema automatically
 Base.metadata.create_all(engine)
+
+def get_party_by_name(party_name):
+	try:
+		party = session.query(Party).filter_by(name = party_name).one()
+	except NoResultFound:
+		party = []
+	return party
 
 def insert_results(county, id_c):
 
 	for r in county.get('results'):
 		name = r.get('name')
 		county_id = id_c
-		new1 = Party(name = name, county_id = county_id)
-		session.add(new1)
-		session.flush()
+		party = get_party_by_name(name)
+		if party != []:
+			party_id = party.id
+		else:
+			new_party = Party(name = name)
+			session.add(new_party)
+			session.flush()
+			session.refresh(new_party)
+			party_id = new_party.id
 
 		e_current = r.get('first').get('current')
 		e_previous = r.get('first').get('previous')
 		z_current = r.get('second').get('current')
 		z_previous = r.get('second').get('previous')
-		new2 = Result(e_current = e_current, e_previous = e_previous, z_current = z_current, z_previous = z_previous, party_id = new1.id)
-		session.add(new2)
+		new_results = Result(e_current = e_current, e_previous = e_previous, z_current = z_current, z_previous = z_previous, party_id = party_id, county_id = id_c)
+		session.add(new_results)
 		session.commit()
 	return 'SUCCESS'
 
-def insert_csv_data():
-	data = csv_reader()
-	json_dump('btw17_data.json', data)
+def insert_counties(data):
 	for c in data:
 		id = c.get('id')
 		belongs_to = c.get('belongs_to')
@@ -81,8 +93,23 @@ def insert_csv_data():
 		if insert_results(c, id) == 'SUCCESS':
 			print('Results inserted for ' + name)
 
-	print('SUCCESS - Provinces/Counties inserted')
 	return 'SUCCESS'
 
-# insert_csv_data()
+def insert_csv_data():
+	data = csv_reader()
+	print('Data import in process...')
+	# json_dump('btw17_data.json', data)
+	if insert_counties(data) == 'SUCCESS':
+		print('Data import successful')
 
+def get_all_provinces():
+	return session.query(Province).all()
+
+def get_all_counties():
+	return session.query(County).all()
+
+def get_results_by_county_id(county_id):
+	return session.query(Results).filter_by(county_id = county_id)
+
+# Execute only once to create tables and fill database
+insert_csv_data()
