27a3c4e54e7dea8cd27ec0e5f6fd3581573bbd2a
Unknown <s0567742@htw-berlin.de>
2019-01-20 18:04:32 +0100

    Display provinces and states as list items

--- static/js/main_controller.js
+++ static/js/main_controller.js
@@ -1,10 +1,29 @@
 var app = angular.module('elections-app', []);
 
 app.controller("main-controller", function ($scope, $http) {
+  // var provinces = [];
+
     $http.get('/getProvinces').success(function(response) {
-      // var response = JSON.parse(response);
-      console.log(response);
+      // for (var i = 0; i < response.length; i++) {
+      //   provinces[i] = response[i].p_name;
+      // }
+      $scope.provinces = response;
     }).error(function(response) {
       console.log(response);
     });
+
+    $scope.displayCounties = function (id) {
+      var counties = [];
+
+        $http.get('/getCounties').success(function (response) {
+          for (var i = 0; i < response.length; i++) {
+            if (response[i].c_belongs_to == id) {
+              counties[i] = response[i];
+            }
+          }
+          $scope.counties = counties;
+          }).error(function (response) {
+              console.log(response);
+          });
+};
 });
